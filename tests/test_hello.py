b2e1ef89ac45229a13eaeaf27aa937c1d6cb66f5
gd40072 <61653848+gd40072@users.noreply.github.com>
2020-10-11 17:43:09 +0200

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -3,3 +3,17 @@ import hello
 
 def test_says_world():
     assert hello.say_what() == 'world3'
+
+    def test_dummy1():
+    assert '1' == '1'
+    
+     def test_dummy2():
+    assert '2' == '2'
+    
+        def test_dummy3():
+    assert '3' == '3'
+    
+        def test_dummy4():
+    assert '4' == '4'
+    
+    
